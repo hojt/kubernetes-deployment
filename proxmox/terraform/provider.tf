46a7405f7895c954c68adaa9b6f645db4d8a8260
Brett Errington <253641+MrBretticus@users.noreply.github.com>
2024-08-16 20:54:06 +1000

    [ENH] Update telmate/proxmox to 3.0.1-rc3 [ENH] Support API token [ENH] Add template_name var [ENH] Update to Ubuntu 24.04 [FIX] Add missing cloud-init disk

--- proxmox/terraform/provider.tf
+++ proxmox/terraform/provider.tf
@@ -2,15 +2,17 @@ terraform {
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
-      version = "2.9.0"
+      version = "3.0.1-rc3"
     }
   }
 }
 
 provider "proxmox" {
-  pm_parallel       = 1
-  pm_tls_insecure   = true
-  pm_api_url        = var.pm_api_url
-  pm_password       = var.pm_password
-  pm_user           = var.pm_user
+  #pm_parallel         = 1
+  pm_tls_insecure     = true
+  pm_api_url          = var.pm_api_url
+  pm_password         = var.pm_password
+  pm_user             = var.pm_user
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
 }
--- proxmox/terraform/variables.tf
+++ proxmox/terraform/variables.tf
@@ -14,6 +14,18 @@ variable "pm_password" {
   default = ""
 }
 
+variable "pm_api_token_id" {
+  default = ""
+}
+
+variable "pm_api_token_secret" {
+  default = ""
+}
+
 variable "ssh_key_file" {
   default = "~/.ssh/id_rsa.pub"
+}
+
+variable "template_name" {
+  default = "ubuntu-2404-cloudinit-template"
 }
